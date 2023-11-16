1fa5ad4d9b51128a299274693af45be2c2575da3
Casey Tran <caseytran892@gmail.com>
2023-11-15 22:28:15 -0800

    added distortion with opencv

--- opencv_stream.py
+++ opencv_stream.py
@@ -2,6 +2,7 @@ import cv2
 import asyncio
 import socketio
 import base64
+import numpy as np
 
 async def main():
     # create socket.io connection
@@ -12,80 +13,33 @@ async def main():
 
         cap = cv2.VideoCapture(0)
         
+        # Shapes video for fisheye fix adjustment
+        codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        cap.set(6, codec)
+        cap.set(5, 30)
+        cap.set(3, 1920)
+        cap.set(4, 1080)
+        
+        # Camera, fixing fisheye
+        cameraMatrix = np.genfromtxt("./camera_matrix.txt")
+        dist = np.genfromtxt("./distortion.txt")
+        
+        ret, frame = cap.read()
+        h, w = frame.shape[:2]
+        
+        newCameraMatrix, roi = cv2. getOptimalNewCameraMatrix(cameraMatrix, dist, (w, h), 1, (w, h))
+        
         while True:
+            
             ret, frame = cap.read()
 
-            # mask = colorMask(frame)
-            # kernel = np.ones((5,5),np.uint8)
-            # # removes small noise
-            # erosion = cv2.erode(mask,kernel,iterations = 1)
-            # # fills gaps, connects nearby regions
-            # img_dilation = cv2.dilate(erosion, kernel, iterations=1)
+            # undistorts the fisheye frame
+            frame = cv2.undistort(frame, cameraMatrix, dist, None, newCameraMatrix)
+            x, y, w, h = roi
+            frame = frame[y:y+h, x:x+w]
             
-            # # Bitwise-AND mask and original image
-            # res = cv2.bitwise_and(frame,frame, mask= img_dilation)
-
-            # # Find the largest contour in the mask
-            # largest_contour = find_largest_contour(res, 2)
-            
-            # # split frame into quadrants
-            # quadrants = createQuadrants(frame)
-            
-            # if largest_contour is not None:
-            #     # Get the bounding box of the largest contour
-            #     x, y, w, h = cv2.boundingRect(largest_contour)
-
-            #     # Draw a rectangle around the largest contour
-            #     frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
-            #         # Calculates coordinates, by calculating the center of the box that highlights the fish
-            #     coord = findMidpoint(x, x + w, y, y + h)
-            #     quad = checkCoordinate(coord[0], coord[1], quadrants)
-                
-            #     # DEBUG PRINTS
-            #     print("COORD: ", coord)
-            #     print("QUADRANT IT IS IN: ", quad)
-            
-            # bit mask showing
-            # cv2.imshow('bitmask', res)
-            # regular showing with frame
             cv2.imshow('frame', frame)
 
-
-            # DEBUGGING PRINTS
-            # Display the quadrants
-            # print("QUADRANTS")
-            # print("TOPLEFT: ", quadrants[0])
-            # print("TOPRIGHT: ", quadrants[1])
-            # print("BOTTOMLEFT: ", quadrants[2])
-            # print("BOTTOMRIGHT: ", quadrants[3])
-
-            # actualQuad = checkCoordinate(coords[0], coords[1], quadrants)
-            # print("QUADRANT IT IS IN: ", actualQuad)
-            # creating box to highlight fish
-            # mask_ = Image.fromarray(mask)
-            
-            # bbox = mask_.getbbox()
-            
-            # if bbox is not None:
-            #     x1, y1, x2, y2 = bbox
-                
-            #     # DEBUG PRINTS
-            #     # print("x1: ", x1)
-            #     # print("x2: ", x2)
-            #     # print("y1: ", y1)
-            #     # print("y2: ", y2)
-                
-            #     # Calculates coordinates, by calculating the center of the box that highlights the fish
-            #     coord = findMidpoint(x1, x2, y1, y2)
-            #     quad = checkCoordinate(coord[0], coord[1], quadrants)
-                
-            #     # DEBUG PRINTS
-            #     print("COORD: ", coord)
-            #     print("QUADRANT IT IS IN: ", quad)
-                
-            #     # Creates the frame around the goldfish
-            #     frameBox = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-            # cv2.imshow('frame', frame)
 
             # 30 ms delay
             key = cv2.waitKey(30) & 0xFF
